b84c032e4346e64b0e70ee9e6ffd919e389cee73
Janvier ISABANE <isabanej@gmail.com>
2020-03-05 22:34:26 -0800

    V1.0.0

---
+++ src/main/java/com/flygram/FlyinggramApplication.java
@@ -0,0 +1,14 @@
+package com.flygram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FlyinggramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyinggramApplication.class, args);
+	}
+
+}
